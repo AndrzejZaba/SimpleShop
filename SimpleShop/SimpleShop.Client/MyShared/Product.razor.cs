1d51bf88ed41b46d1e24dedc3d08e37ed9fb47fe
AndrzejZaba <andrzejzaba@onet.pl>
2024-09-09 16:49:09 +0200

    Product component added

---
+++ SimpleShop/SimpleShop.Client/MyShared/Product.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleShop.Shared.Products.Dtos;
+
+namespace SimpleShop.Client.MyShared;
+
+public partial class Product
+{
+    private string _baseUrl = string.Empty;
+
+    [Parameter]
+    public ProductDto ProductModel { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    public IConfiguration Configuration { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _baseUrl = Configuration["ApiConfiguration:BaseAddress"];
+    }
+
+    private async Task OnAddProductToCart()
+    {
+        // dodawanie produktu do koszyka
+
+        NavigationManager.NavigateTo("/koszyk");
+    }
+}
