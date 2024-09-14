96b44e406def7b3d3240a9a300c9717f3d340d4b
AndrzejZaba <andrzejzaba@onet.pl>
2024-09-14 13:52:59 +0200

    Added Cart page

--- SimpleShop/SimpleShop.Client/MyShared/Product.razor.cs
+++ SimpleShop/SimpleShop.Client/MyShared/Product.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
 using SimpleShop.Shared.Products.Dtos;
 
 namespace SimpleShop.Client.MyShared;
@@ -16,6 +17,9 @@ public partial class Product
     [Inject]
     public IConfiguration Configuration { get; set; }
 
+    [Inject]
+    public ILocalStorageService LocalStorage { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
         _baseUrl = Configuration["ApiConfiguration:BaseAddress"];
@@ -24,6 +28,14 @@ public partial class Product
     private async Task OnAddProductToCart()
     {
         // dodawanie produktu do koszyka
+        var products = await LocalStorage.GetItemAsync<List<ProductDto>>("cart");
+
+        if (products == null)
+            products = new List<ProductDto>();
+
+        products.Add(ProductModel);
+
+        await LocalStorage.SetItemAsync("cart", products);
 
         NavigationManager.NavigateTo("/koszyk");
     }
---
+++ SimpleShop/SimpleShop.Client/Pages/Cart.razor.cs
@@ -0,0 +1,69 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using SimpleShop.Shared.Products.Dtos;
+
+namespace SimpleShop.Client.Pages;
+
+public partial class Cart
+{
+    private List<ProductDto> _products;
+    private string _baseUrl = string.Empty;
+    private decimal _totalValue = 0;
+
+    [Inject]
+    public IConfiguration Configuration { get; set; }
+
+    [Inject]
+    public ILocalStorageService LocalStorage { get; set; }
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _baseUrl = Configuration["ApiConfiguration:BaseAddress"];
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _products = await LocalStorage.GetItemAsync<List<ProductDto>>("cart");
+
+            if (_products == null)
+                _products = new List<ProductDto>();
+            
+            _totalValue = _products.Select(x => x.Price).Sum();
+
+            StateHasChanged();
+        }
+    }
+
+    private async void OnDeleteProductFromCart(int id)
+    {
+        var productToDelete = _products.FirstOrDefault(x => x.Id == id);
+
+        if (productToDelete == null)
+            return;
+
+        _products.Remove(productToDelete);
+
+        _totalValue = _products.Select(x => x.Price).Sum();
+
+        await LocalStorage.SetItemAsync("cart", _products);
+    }
+
+    private void GoHome()
+    {
+        NavigationManager.NavigateTo("/");
+    }
+    
+    private void ConfirmOrder()
+    {
+        if (!_products.Any())
+            return;
+
+        NavigationManager.NavigateTo("/zamowienie");
+
+    }
+}
