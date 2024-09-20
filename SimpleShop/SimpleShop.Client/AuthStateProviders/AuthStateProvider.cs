5bc6956e081adbece6ebda90ca6f897437040b5f
AndrzejZaba <andrzejzaba@onet.pl>
2024-09-20 12:30:32 +0200

    AuthStateProvider changes

--- SimpleShop/SimpleShop.Client/AuthStateProviders/AuthStateProvider.cs
+++ SimpleShop/SimpleShop.Client/AuthStateProviders/AuthStateProvider.cs
@@ -1,23 +1,45 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using SimpleShop.Client.Models;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 
 namespace SimpleShop.Client.AuthStateProviders;
 
 public class AuthStateProvider : AuthenticationStateProvider
 {
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorage;
+    private readonly AuthenticationState _anonymous;
+
+    public AuthStateProvider(
+        HttpClient httpClient, 
+        ILocalStorageService localStorage)
+    {
+        _httpClient = httpClient;
+        _localStorage = localStorage;
+        _anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+    }
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        var anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        var token = await _localStorage.GetItemAsync<string>("authToken");
 
-        var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.Name, "aswen124@gmail.com"),
-            new Claim(ClaimTypes.Role, "Administrator"),
-        };
+        if (string.IsNullOrWhiteSpace(token)) 
+            return _anonymous;
 
-        var auth = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, "test")));
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+
+        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
+
+        //var claims = new List<Claim>
+        //{
+        //    new Claim(ClaimTypes.Name, "aswen124@gmail.com"),
+        //    new Claim(ClaimTypes.Role, "Administrator"),
+        //};
+
+        //var auth = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, "test")));
 
 
-        return await Task.FromResult(auth);
+        //return await Task.FromResult(auth);
     }
 }
