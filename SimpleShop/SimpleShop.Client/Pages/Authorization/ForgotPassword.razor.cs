8a044cac391cd759ea930d62322475c793a5582e
AndrzejZaba <andrzejzaba@onet.pl>
2024-09-21 22:16:53 +0200

    Reset Password

---
+++ SimpleShop/SimpleShop.Client/Pages/Authorization/ForgotPassword.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleShop.Client.HttpRepository.Interfaces;
+using SimpleShop.Shared.Authentication.Commands;
+using System.Net;
+
+namespace SimpleShop.Client.Pages.Authorization
+{
+    public partial class ForgotPassword
+    {
+        private ForgotPasswordCommand _command = new ForgotPasswordCommand();
+        private bool _showSuccess;
+        private bool _showError;
+
+        [Inject]
+        public IAuthenticationHttpRepository AuthenticationRepo { get; set; }
+
+        private async Task Save()
+        {
+            _showSuccess = _showError = false;
+
+            var result = await AuthenticationRepo.ForgotPassword(_command);
+
+            if (result == HttpStatusCode.OK)
+                _showSuccess = true;
+            else
+                _showError = true;
+        }
+    }
+}
